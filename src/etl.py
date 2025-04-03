fbe2964b735e6c16fb70c16fe773cbd47a7c6315
LittleCriis <cristina_alvarez1@msn.com>
2025-04-03 12:18:27 +0200

    Update etl.py

--- src/etl.py
+++ src/etl.py
@@ -1,10 +1,11 @@
 import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 #Leemos el csv con el que vamos a trabajar y lo guardamos en un dataframe
-df=pd.read_csv('C:/Users/Cristina/Desktop/MASTER DATA/Python/Wproject/data/datosINE.csv', sep=';', encoding='ISO-8859-1', decimal=',')
+df = pd.read_csv('C:/Users/Cristina/Desktop/MASTER DATA/Python/Wproject/data/datosINE.csv', sep=';', encoding='ISO-8859-1')
+#Sustituir las celdas NaN por un string vacío
+df = df.fillna('')
+# Renombrar columnas "Unnamed: X" con un string vacío
+df.columns = [col if not col.startswith('Unnamed:') else '' for col in df.columns]
 #Mostramos el dataframe
 print(df.head(10))
 #Mostramos la información del dataframe
@@ -13,13 +14,7 @@ df.info()
 df.describe()
 #Mostramos la cantidad de filas y columnas del dataframe
 df.shape
-#Mostramos los nombres de las columnas del dataframe
-df.columns
 #Mostramos los tipos de datos del dataframe
 df.dtypes
 
-#Eliminamos las columnas y filas que no nos interesan como Unnamed y NaN
-df.drop(columns=['Unnamed: 0', 'Unnamed: 1'], inplace=True)
-df.dropna(inplace=True)
-#Mostramos el dataframe 
-print(df.head(10))
+
