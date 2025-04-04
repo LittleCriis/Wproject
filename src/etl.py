224faa0048a1697c64da52585eb5bcd6573f8b6b
LittleCriis <cristina_alvarez1@msn.com>
2025-04-04 10:02:21 +0200

    Update etl.py

--- src/etl.py
+++ src/etl.py
@@ -2,10 +2,6 @@ import pandas as pd
 
 #Leemos el csv con el que vamos a trabajar y lo guardamos en un dataframe
 df = pd.read_csv('C:/Users/Cristina/Desktop/MASTER DATA/Python/Wproject/data/datosINE.csv', sep=';', encoding='ISO-8859-1')
-#Sustituir las celdas NaN por un string vacío
-df = df.fillna('')
-# Renombrar columnas "Unnamed: X" con un string vacío
-df.columns = [col if not col.startswith('Unnamed:') else '' for col in df.columns]
 #Mostramos el dataframe
 print(df.head(10))
 #Mostramos la información del dataframe
@@ -16,5 +12,13 @@ df.describe()
 df.shape
 #Mostramos los tipos de datos del dataframe
 df.dtypes
-
-
+#Eliminamos todas las filas hasta la número 6
+df = df.iloc[6:]
+#Sustituir las celdas NaN por un string vacío
+df = df.fillna('')
+# Renombrar columnas "Unnamed: X" con un string vacío
+df.columns = [col if not col.startswith('Unnamed:') else '' for col in df.columns]
+# Colocar "Años" solo en la celda A9 (fila 8, columna 0)
+df.at[7, df.columns[0]] = 'Años'
+#Mostramos el dataframe 
+print(df.head(10))
